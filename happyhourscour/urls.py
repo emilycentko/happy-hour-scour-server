4ad1edb1f0389bc882d8f6843341e8f80d1e94ef
emilycentko <emily.centko@gmail.com>
2021-06-11 09:45:19 -0500

    Finished building out list to add router to urls and import happy hour view in init file

--- happyhourscour/urls.py
+++ happyhourscour/urls.py
@@ -1,8 +1,14 @@
+from rest_framework import routers
+from happyhourscourapi.views import HappyHourView
 from django.conf.urls import include
 from django.urls import path
 from happyhourscourapi.views import register_user, login_user
 
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'happyhours', HappyHourView, 'happyhour')
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
