6a539d9e0c93c9d151e8fa4ed0ea24dd78938551
emilycentko <emily.centko@gmail.com>
2021-06-24 10:08:52 -0500

    Updated settings for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn happyhourscour.wsgi --log-file -
--- happyhourscour/settings.py
+++ happyhourscour/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import environ
+import django_on_heroku
 from pathlib import Path
 
 env = environ.Env()
@@ -153,9 +154,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+django_on_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
