bc074f7fd2ed5ea58efcb300490fe8208d036e98
emilycentko <emily.centko@gmail.com>
2021-06-24 12:11:52 -0500

    Added admin route

--- happyhourscour/urls.py
+++ happyhourscour/urls.py
@@ -4,6 +4,7 @@ from happyhourscourapi.views import HappyHourView, Profile, FavoriteView, Specia
 from django.conf.urls import include
 from django.urls import path
 from happyhourscourapi.views import register_user, login_user
+from django.contrib import admin
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'happyhours', HappyHourView, 'happyhour')
@@ -18,4 +19,5 @@ urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('admin/', admin.site.url)
 ]
