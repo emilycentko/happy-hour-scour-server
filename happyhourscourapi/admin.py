35e5175cfeef566148121cf7115c981ee130f260
emilycentko <emily.centko@gmail.com>
2021-06-24 15:42:36 -0500

    Loaded models in admin

--- happyhourscourapi/admin.py
+++ happyhourscourapi/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models.customer import Customer
+from .models.business import Business
+from .models.weekday import WeekDay
+from .models.business_type import BusinessType
+from .models.happy_hour import HappyHour
+from .models.favorite import Favorite
+from .models.special_type import SpecialType
+from .models.review import Review
+from .models.location import Location
 
 # Register your models here.
+
+admin.site.register(HappyHour)
+admin.site.register(Favorite)
