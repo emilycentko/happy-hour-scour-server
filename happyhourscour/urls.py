d86aec1e031458e97aabc0125b7c576d76fd85df
emilycentko <emily.centko@gmail.com>
2021-06-24 12:15:31 -0500

    Fixed urls

--- happyhourscour/urls.py
+++ happyhourscour/urls.py
@@ -19,5 +19,5 @@ urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.url)
+    path('admin/', admin.site.urls)
 ]
