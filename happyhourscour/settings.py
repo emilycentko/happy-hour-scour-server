a0c3eb17092c50e3aa591b2502a6c6cd0640f1f3
emilycentko <emily.centko@gmail.com>
2021-06-24 13:20:12 -0500

    Updated CORS

--- happyhourscour/settings.py
+++ happyhourscour/settings.py
@@ -73,7 +73,9 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://happy-hour-scour.netlify.app'
+    'https://happy-hour-scour.herokuapp.com'
 )
 
 MIDDLEWARE = [
